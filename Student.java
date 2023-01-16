67953332e13ca136ef6c79e3d55406cf9346f4a6
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-17 00:47:31 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,36 +1,21 @@
 //  Write a Java program to enter marks of five subjects and calculate total, average and percentage.
 
-import java.util.Scanner;
 
 class Student
  {  
    public static void main (String args[])  
     {      
-		Scanner sc = new Scanner(System.in);
-		double sub1, sub2, sub3, sub4, sub5 , total;  
-		System.out.println("Enter sub1 marks");
-		sub1 = sc.nextDouble();
-
-		System.out.println("Enter sub2 marks");
-		sub2 = sc.nextDouble();
-
-		System.out.println("Enter sub3 marks");
-		sub3 = sc.nextDouble();
-
-		System.out.println("Enter sub4 marks");
-		sub4 = sc.nextDouble();
-
-		System.out.println("Enter sub5 marks");
-		sub5 = sc.nextDouble();
-
-		double avg, percentage;
-			
+		int sub1, sub2, sub3, sub4, sub5 , total;  
+		float avg, percentage;
+			sub1 = 86;
+			sub2 = 98;
+			sub3 = 45;
+			sub4 = 65;
+			sub5 = 72;
 
 			total = sub1+sub2+sub3+sub4+sub5;
-			
-			avg = total / 5;
-
 			percentage = (total / 500) * 100;	
+			avg = total / 5;
 
 		System.out.println("Total marks of student= "+total);
 		System.out.println("Average marks of student= "+avg);
