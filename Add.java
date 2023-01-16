8dffbb6157ba723e61a6a36800a8699514ef941f
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-16 11:44:45 +0530

    Add files via upload
    Addition program is completed.

---
+++ Add.java
@@ -0,0 +1,20 @@
+//Write a Java program to enter two numbers and perform all arithmetic operations.
+
+class Add
+ {  
+   public static void main (String args[])  
+    {      
+		int num1, num2, add,sub,mul;  
+		float div;
+                  num1= 40; 
+                  num2= 26;
+
+                 add= num1+num2;
+			sub= num1-num2;
+			mul=num1*num2;
+			div=num1/num2;
+            System.out.println("Sum of Two number is "+add); 
+		System.out.println("Subtraction of Two number is "+sub);
+		System.out.println("Multiplication of Two number is "+mul);
+		System.out.println("Division of Two number is "+div); 
+    }}  
