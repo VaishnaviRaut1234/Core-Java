bba391a441f53a593669a53776bad369356be9db
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-17 00:53:47 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,17 @@
+// Write a Java program to Swap values of two integer variables using thirdvariable.
+
+class Swap
+ {  
+   public static void main (String args[])  
+    {      
+		int num1, num2, Temp;  
+		
+                  num1= 40; 
+                  num2= 26;
+			Temp=0;
+			
+			num1 = num2;
+			num2 = Temp;
+			Temp = num1;
+            System.out.println("After swapping: "+num1 +"   " + num2);   
+    }}  
