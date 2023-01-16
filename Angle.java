26754795e4036f2a38ffa024ad05fdecefb17ff6
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-16 22:59:31 +0530

    Add files via upload

---
+++ Angle.java
@@ -0,0 +1,16 @@
+// Write a Java program to enter two angles of a triangle and find the third angle.
+
+
+class Angle
+ {  
+   public static void main (String args[])  
+    {      
+		int angle1, angle2, angle3;  
+			angle1 = 90;
+			angle2 = 20;
+		
+			angle3 = 180 - (angle1 + angle2);			
+
+		System.out.println("Angle3 = "+angle3);
+		
+    }}  
