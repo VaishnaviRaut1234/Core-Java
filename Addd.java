932fab14087f5c4c07ad9379892a10d54582a8f6
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-17 00:50:32 +0530

    Add files via upload

---
+++ Addd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Addd {
+public static void main(String[] args) {
+//Scanner sc = new Scanner(System.in);
+
+short a = 5 ;
+short b =20 ;
+
+int sum = (int) (a+b);
+System.out.println("sum = "+sum);
+}
+}
