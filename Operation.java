dc41841fb592a448b7b1eeb3b65b220ea8dcba04
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-17 00:46:17 +0530

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,10 @@
+class Operation
+{
+public static void main(String args[])
+{
+double ans;
+ans = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+
+System.out.println("Answer is " +ans);
+}
+}
