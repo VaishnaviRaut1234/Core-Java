174d87c699b3ea54108ce6e161a7cab34c766588
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-16 23:00:16 +0530

    Add files via upload

---
+++ Areat.java
@@ -0,0 +1,16 @@
+//  Write a Java program to enter base and height of a triangle and find its area.
+
+class Areat
+ {  
+   public static void main (String args[])  
+    {      
+		int base, height;  
+		float area;
+			base = 7;
+			height = 15;
+		
+			area = (base * height)/2;			
+
+		System.out.println("Area of Triangle= "+area);
+		
+    }}  
