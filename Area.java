ea5bf29edae8c72084d9c2deb7648683428d89cc
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-16 22:59:51 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,12 @@
+// Write a Java program to enter length and breadth of a rectangle and find its area.
+
+class Area
+ {  
+   public static void main (String args[])  
+    {      
+		int length, breadth, area;  
+                  length= 40; 
+                  breadth= 26;
+                 area= length*breadth;
+            System.out.println("Area of rectangle is "+area);  
+    }}  
