8fb2286910070a2d8e7c055e212553ae66062247
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-17 02:29:08 +0530

    Add files via upload

---
+++ DemoOnOperators.java
@@ -0,0 +1,12 @@
+public class  DemoOnOperators
+{
+	public static void main(String args[])
+	{
+		int input = 7;
+		int output = ++input + ++input + ++input;
+
+
+		System.out.println(output);
+		
+}
+}
