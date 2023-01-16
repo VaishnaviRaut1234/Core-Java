dafb36ac612da3087adaf8c6c4d954b63179ba7a
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-16 23:00:35 +0530

    Add files via upload

---
+++ Areatet.java
@@ -0,0 +1,15 @@
+//  Write a Java program to calculate area of an equilateral triangle.
+
+class Areaet
+ {  
+   public static void main (String args[])  
+    {      
+		int side;  
+		double area;
+			side = 10;
+		
+			area = (1.73 * side * side)/4;			
+
+		System.out.println("Area of Equilateral Triangle= "+area);
+		
+    }}  
