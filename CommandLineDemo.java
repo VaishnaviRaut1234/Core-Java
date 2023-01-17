db4242ff62712c5f992f1fc3444a22b546c24ad5
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-17 17:43:33 +0530

    Add files via upload

--- CommandLineDemo.java
+++ CommandLineDemo.java
@@ -2,10 +2,10 @@ class CommandLineDemo
 {
 	public static void main(String args[])
 	{
-		//System.out.println("Enter first name");
+		System.out.println("Enter first name");
 		String fname = args[0];
 	
-		//System.out.println("Enter last name");
+		System.out.println("Enter last name");
 		String lname = args[1];
 
 		System.out.println("Welcome to "+fname+ " " +lname);
