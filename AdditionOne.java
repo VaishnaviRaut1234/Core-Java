39ef4327f01f923ae7baa4641b1356f588d5cda5
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-17 17:43:47 +0530

    Add files via upload

---
+++ AdditionOne.java
@@ -0,0 +1,17 @@
+class AdditionOne
+{
+	public static void main(String args[])
+	{
+		int a = 4;
+		a++;
+		a +=a;
+		--a;
+		a = 7 +a;
+		a *= a;
+		a -=3;
+		
+		System.out.println("a = "+a);
+
+		
+}
+}
