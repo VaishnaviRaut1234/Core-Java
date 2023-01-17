14ce644d9e337ab3053d8fbd0c020dc0f34ab984
VaishnaviRaut1234 <104056263+VaishnaviRaut1234@users.noreply.github.com>
2023-01-17 17:38:27 +0530

    Add files via upload

---
+++ CommandLineDemo.java
@@ -0,0 +1,14 @@
+class CommandLineDemo
+{
+	public static void main(String args[])
+	{
+		//System.out.println("Enter first name");
+		String fname = args[0];
+	
+		//System.out.println("Enter last name");
+		String lname = args[1];
+
+		System.out.println("Welcome to "+fname+ " " +lname);
+		
+	}
+}
