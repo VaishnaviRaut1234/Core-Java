92d8e782bed9b21769ec8b28c0c5e57d227e18ee
HP <HP@LAPTOP-KUVAU00N>
2023-03-27 10:00:55 +0530

    Done

---
+++ PracticeAssignment/src/com/practice/Practice.java
@@ -0,0 +1,24 @@
+package com.practice;
+import java.util.Scanner;
+
+public class Practice 
+{
+	public static void main(String args[])
+	{
+	Scanner sc = new Scanner(System.in);
+	  long binaryNumber, decimalNumber = 0, j = 1, remainder;
+	  System.out.println("Input a binary number: ");
+	  binaryNumber = sc.nextLong();
+	  
+	  while (binaryNumber != 0) 
+	  {
+	   remainder = binaryNumber % 10;
+	   decimalNumber = decimalNumber + remainder * j;
+	   j = j * 2;
+	   binaryNumber = binaryNumber / 10;
+	  }
+	
+	  System.out.println("Decimal Number: " + decimalNumber);
+	 }
+	  
+}
---
+++ PracticeAssignment/src/com/practice/String.java
@@ -0,0 +1,15 @@
+package com.practice;
+import java.util.Scanner;
+
+public class String 
+{	   
+	public static void main(String[] args)  
+	{  
+		Scanner sc= new Scanner(System.in); 
+		System.out.print("Enter a string: ");  
+		 java.lang.String str= sc.next();
+		System.out.print("You have entered: "+str);             
+}  
+}  
+
+
